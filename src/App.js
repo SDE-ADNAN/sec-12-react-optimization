a18d0ce5a52de22d8bf4594e5d1075d230b2a0b2
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-11-30 20:51:41 +0530

    added the code for statechange rerender of a component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import './App.css';
+import "./App.css";
+import Button from "./components/UI/Button/Button";
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false); // when the state changes, the component will re-render
+  // whenever the showParagraph changes, the component will re-render
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {showParagraph && <p>This is New!</p>}
+      <Button onClick={() => setShowParagraph(!showParagraph)}>
+        Toggle Paragraph!
+      </Button>
     </div>
   );
 }
