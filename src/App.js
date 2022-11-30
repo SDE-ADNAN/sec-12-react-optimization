ea1f697ca70c058465eb06d09767204fbe43db85
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-11-30 22:34:00 +0530

    understood why we should use console.log statements rather than using the flashes on the browsers elements tab , also understood about why the parent rerenders when ever their is its state changed and atlast understood that childs rerender whenever props gets changed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { useState } from "react";
 
 import "./App.css";
+import DemoOutput from "./components/Demo/DemoOutput";
 import Button from "./components/UI/Button/Button";
 
 function App() {
+  console.log("APP RUNNING");
   const [showParagraph, setShowParagraph] = useState(false); // when the state changes, the component will re-render
   // whenever the showParagraph changes, the component will re-render
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {showParagraph && <p>This is New!</p>}
+      <DemoOutput show={showParagraph} />
       <Button onClick={() => setShowParagraph(!showParagraph)}>
         Toggle Paragraph!
       </Button>
